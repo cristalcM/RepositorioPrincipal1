feb29d5e3d996acf7b80dfbdef664019c864739a
cristalcM <maria.murillo1065@alumnos.udg.mx>
2024-09-16 02:03:25 -0600

    Menu principal y configuración
    cambie sonido, interfaz y mas

---
+++ new game I/Assets/Scripts/interfaz/MainPanel.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MainPanel : MonoBehaviour
+{
+
+    //--------------------------
+    [Header("Ajustes")]
+    //---------------------------
+    public Slider VolumenFX;
+    public Slider volumenMaster;
+    public AudioMixer mixer;
+
+    public AudioSource fxsourse;
+    public AudioClip clicksound;
+
+    //------------------------
+    [Header("Panels")]
+    //---------------------------
+    public GameObject mainPanel;
+    public GameObject ajustesPanel;
+
+    private void Awake()
+    {
+        VolumenFX.onValueChanged.AddListener(ChangeVolumenFX);
+        volumenMaster.onValueChanged.AddListener(ChangeVolumenmaster);
+
+
+        LoadVolumeSettings();
+    }
+
+
+    //---------------------------------------
+    //Metodo para  abrir y cambiar de panel
+    //---------------------------------------
+    public void OpenPanel(GameObject panel)
+    {
+        //iniciaizar
+        mainPanel.SetActive(false);
+        ajustesPanel.SetActive(false);
+
+        panel.SetActive(true);
+
+        PLaySoundButton();
+        
+    }
+    //---------------------------------------
+    //Método para cambiar de escena al presionar el botón Play
+    //---------------------------------------
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    //---------------------------------------
+    //Metodo para ajustar los volumenes 
+    //---------------------------------------
+    public void ChangeVolumenmaster(float V)
+    {
+        mixer.SetFloat("VolMaster", V);
+        PlayerPrefs.SetFloat("VolMaster", V);
+    }
+
+    public void ChangeVolumenFX(float V)
+    {
+        mixer.SetFloat("VolFX", V);
+        PlayerPrefs.SetFloat("VolFX", V);  // Guardar el valor de volumen FX
+    }
+
+    public void PLaySoundButton()
+    {
+        fxsourse.PlayOneShot(clicksound);
+    }
+    //---------------------------------------
+    //Método para cargar los valores de volumen guardados
+    //---------------------------------------
+    private void LoadVolumeSettings()
+    {
+        if (PlayerPrefs.HasKey("VolMaster"))
+        {
+            float volMaster = PlayerPrefs.GetFloat("VolMaster");
+            volumenMaster.value = volMaster;
+            mixer.SetFloat("VolMaster", volMaster);
+        }
+
+        if (PlayerPrefs.HasKey("VolFX"))
+        {
+            float volFX = PlayerPrefs.GetFloat("VolFX");
+            VolumenFX.value = volFX;
+            mixer.SetFloat("VolFX", volFX);
+        }
+    }
+
+
+}
