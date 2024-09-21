ef996d966d7da9be4e5addd78ae163e62c2965a3
cristalcM <maria.murillo1065@alumnos.udg.mx>
2024-09-20 21:57:04 -0600

    Diálogos y Movimiento de cámara

--- new game I/Assets/Scripts/Logica del juego/Aike.cs
+++ new game I/Assets/Scripts/Logica del juego/Aike.cs
@@ -1,5 +1,9 @@
+using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Aike : MonoBehaviour
@@ -8,15 +12,15 @@ public class Aike : MonoBehaviour
     public GameObject sombrilla;
     public GameObject tazaPrefab;
     private bool jugadorEnRango = false;  // Para detectar si el jugador está cerca
-
+    public DialogoNPC Dialogo;
 
     private void Update()
     {
         if (jugadorEnRango && Input.GetKeyDown(KeyCode.E)) // Si el jugador está en rango y presiona E
         {
             Player player = FindFirstObjectByType<Player>();
-            DarSombrilla(player.TieneSombra());  // Llama a DarComida si el jugador está cerca
-            //player.PermitirRecibirSombra();
+            DarSombrilla(player.TieneSombra());  // Llama a DarSomvrilla si el jugador está cerca
+            
         }
     }
 
@@ -44,9 +48,12 @@ public class Aike : MonoBehaviour
         Debug.Log("Texto de conversaion");
         if (necesitaAyuda)
         {
+            Dialogo.MostrarDialogo(AikeDialogoSinSombra);
+
             if ( JugadorTieneSombra)
             {
                 Debug.Log("Gracias por traerme la sombrilla.");
+                Dialogo.MostrarDialogo(AikeDialogoConSombra);
                 necesitaAyuda = false;
                 CrearTaza();  // Crear taza en el mundo();
             }
@@ -59,6 +66,7 @@ public class Aike : MonoBehaviour
         else
         {
             Debug.Log("Ya no necesito ayuda.");
+            Dialogo.MostrarDialogo(AikeDialogoConAyuda);
         }
     }
 
@@ -68,5 +76,42 @@ public class Aike : MonoBehaviour
             Debug.Log("Aike ha dejado una taza.");
             Instantiate(tazaPrefab, transform.position + new Vector3(1, 0, 0), Quaternion.identity);  // Aparece la taza
         }
-    
+
+
+
+    //__________________________________________
+    //SUS DIALOGOS 
+    //_________________________________________
+    [SerializeField, TextArea(4, 6)]
+    private string[] AikeDialogoSinSombra =
+    {
+        "Jugador: ¿Necesitas ayuda?",
+        "Aike: Sí, por favor, no logro alcanzar mi sombrilla y la necesito para protegerme del sol cuando vaya a mi salón.",
+        "Jugador: Claro, yo te ayudo."
+    };
+
+    [SerializeField, TextArea(4, 6)]
+    private string[] AikeDialogoConSombra=
+    { 
+       "Jugador: Aquí tienes",
+       "Aike: ¡Muchas gracias! Ahora podré salir sin problemas.",
+       "Jugador: Me alegra haber podido ayudarte.",
+       "Aike: Ten, es un pequeño obsequio. Note que has estado ayudando al gatito, así que espero que te sea de utilidad.",
+       "*Le entrega una taza en agradecimiento.",
+       "Jugador: ¡Oh, una taza! Muchas gracias, ahora ya no tendré que hacer dos viajes para poder darle de comer."
+    };
+
+    [SerializeField, TextArea(4, 6)]
+    private string[] AikeDialogoConAyuda =
+    {
+       "Aike: ¡Hey! Es bueno volver a verte; gracias a ti llegué a tiempo a mi clase.",
+       "Jugador: No hay de qué, sé que habrías hecho lo mismo por mí.",
+       "Aike:  :)"
+    };
+
+
+
+
+
+
 }
